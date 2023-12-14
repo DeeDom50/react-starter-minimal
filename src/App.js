60340e8a29719b7c6b127f9963b4dc39a1378f23
raedownen <rae.l.downen@gmail.com>
2023-12-14 16:32:58 -0600

    EOD react lesson 12/14/23

--- src/App.js
+++ src/App.js
@@ -4,28 +4,16 @@ import Header from "./components/Header";
 import Button from "./components/Button";
 import Counter from "./components/Counter";
 import BigCircle from "./components/BigCircle";
+import Hero from "./components/Hero";
+import MoviesContainer from "./components/MoviesContainer";
 
 const App = () => {
 	return (
 		<div className="app-wrapper">
 			<Header />
+			<Hero />
 			<div className="container">
-				<div className="row">
-					<div className="col d-flex text-center justify-content-center">
-						<h1>Visual Design & Web Development</h1>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col d-flex text-center justify-content-center">
-						<h2>From Park City, Utah</h2>
-					</div>
-				</div>
-				<div className="d-flex align-items-center flex-wrap justify-content-center">
-					<BigCircle text="Design" color="#ES563B" />
-					<BigCircle text="Develop" color="#EB9D3A" />
-					<BigCircle text="Support" color="#7BB06C" />
-					<BigCircle text="Host" color="#00AAA9" />
-				</div>
+				<MoviesContainer />
 			</div>
 		</div>
 	);
